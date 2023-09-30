534083844761b7690b204310b38141cce75e0a90
James <102871516+powersja@users.noreply.github.com>
2023-09-30 17:02:19 -0400

    Revert "Dynamic header and footer again"
    This reverts commit 05729cbc4930821c3fc2651149e4dfc375ac769d.

--- resources/pageLayout.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('#headerContainer').load('./resources/header.html');
-    $('#navbarContainer').load('./resources/navbar.html');
-    $('#footerContainer').load('./resources/footer.html');
-});
